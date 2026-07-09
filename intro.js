7f87f2232b3c25cd60aae7c9a5a94ee75b1f5abb
iamrobert <robertawiesa@gmail.com>
2026-07-09 15:56:54 +0000

    Intro commit: my name, age and favourite programing language

---
+++ intro.js
@@ -0,0 +1,9 @@
+const mySelf = {
+    name: "Robert Awiesa",
+    age: 22,
+    favProLang: "JavaScript"
+}
+
+console.log("My name is " + mySelf.name + ".")
+console.log("I am " + mySelf.age + " years old.")
+console.log("My favourite programing language is " + mySelf.favProLang + ".")
